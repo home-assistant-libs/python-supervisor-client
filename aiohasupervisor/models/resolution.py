ef8db3975500300d160117a1fd9e41ed47af663d
Stefan Agner <stefan@agner.ch>
2025-08-19 23:35:14 +0200

    Add duplicate OS installation unhealthy reason

--- aiohasupervisor/models/resolution.py
+++ aiohasupervisor/models/resolution.py
@@ -117,10 +117,11 @@ class UnhealthyReason(StrEnum):
     """
 
     DOCKER = "docker"
+    DUPLICATE_OS_INSTALLATION = "duplicate_os_installation"
     OSERROR_BAD_MESSAGE = "oserror_bad_message"
     PRIVILEGED = "privileged"
-    SUPERVISOR = "supervisor"
     SETUP = "setup"
+    SUPERVISOR = "supervisor"
     UNTRUSTED = "untrusted"
 
 
